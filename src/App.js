69d8db36478ceda6156c27c57c7ac4cce7a0cfa9
hasan <hasanakoz1986@gmail.com>
2022-11-10 21:40:29 +0300

    counter increment, decrement,reset done with redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Counter from "./components/counter/Counter";
 import Todo from "./components/todo/Todo";
+import { legacy_createStore as createStore } from "redux";
+import reducer from "./redux";
+import { Provider } from "react-redux";
 
 function App() {
+  const store = createStore(reducer);
   return (
     <div className="app">
-      <Counter />
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+
       <Todo />
     </div>
   );
